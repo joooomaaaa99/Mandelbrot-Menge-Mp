466f2ae507f552e1d3b684a5ae8f1b4efb953796
joooomaaaa99 <jonasmartin99@gmx.de>
2017-02-07 16:18:20 +0100

    Create MandelbrotMain.py

---
+++ MandelbrotMain.py
@@ -0,0 +1,16 @@
+
+"""!/usr/bin/python
+-*- coding: UTF-8 -*-
+Date: 02.02.2017
+"""
+
+import math 
+import tkinter
+
+
+fenster = tkinter.Tk()
+fenster.title("Die Mandelbrot-Menge")
+w = tkinter.Canvas(fenster, width=xwidth, height=yheight)
+w.pack()
+
+tk.mainloop()
