1b95e667bcb79e1b3db37753c227679a09213c2a
joooomaaaa99 <jonasmartin99@gmx.de>
2017-02-07 16:49:54 +0100

    Update MandelbrotMain.py

--- MandelbrotMain.py
+++ MandelbrotMain.py
@@ -21,13 +21,25 @@ def callback():
     w.get()
     return()
 
+#Schritte werden berechnet
+def deltacalc(xmin, xmax, ymin, ymax):
+    deltax = (xmax - xmin)/500
+    deltay = (ymax - ymin)/500
+
+
+
 while True:
-  w.focus_set()
-  xmin = tkinter.Button(fenster, text = "xMin eingeben", width = 10, command = callback)
-  xmax = tkinter.Button(fenster, text = "xMax eingeben", width = 10, command = callback)
-  ymin = tkinter.Button(fenster, text = "yMin eingeben", width = 10, command = callback)
-  ymax = xmax - xmin + ymin
-  
-  
-  
+    w.focus_set()
+    xmin = tkinter.Button(fenster, text = "xMin eingeben", width = 10, command = callback)
+    xmax = tkinter.Button(fenster, text = "xMax eingeben", width = 10, command = callback)
+    ymin = tkinter.Button(fenster, text = "yMin eingeben", width = 10, command = callback)
+    ymax = xmax - xmin + ymin
+    deltacalc()
+    #Durchgehen aller x-Werte
+    for x in range(0, 500):
+        #Berechnung cr
+        #Durchgehen aller y-Werte
+        for y in range(0, 500):
+            #Berechnung ci
+    
   w.pack()
