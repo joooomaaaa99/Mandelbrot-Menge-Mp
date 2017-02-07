c5cef2ab39a82c2746f938f1560d9339500127ea
joooomaaaa99 <jonasmartin99@gmx.de>
2017-02-07 16:57:59 +0100

    Update MandelbrotMain.py

--- MandelbrotMain.py
+++ MandelbrotMain.py
@@ -7,8 +7,16 @@ Date: 07.02.2017
 import math 
 import tkinter
 
+#Fensterbreite
 xwidth = 500
 yheigt = 500
+
+#Iterationsschritte, initialisierung
+n = 0
+
+#Betrag, Abstand zur 0
+betrag = 0
+
 
 fenster = tkinter.Tk()
 fenster.title("Die Mandelbrot-Menge.")
@@ -16,7 +24,7 @@ w = tkinter.Canvas(fenster, width=xwidth, height=yheight)
 w.pack()
 #tk.mainloop()
 
-
+#Rückgabe der Eingabewerte
 def callback():
     w.get()
     return()
@@ -33,6 +41,7 @@ while True:
     xmin = tkinter.Button(fenster, text = "xMin eingeben", width = 10, command = callback)
     xmax = tkinter.Button(fenster, text = "xMax eingeben", width = 10, command = callback)
     ymin = tkinter.Button(fenster, text = "yMin eingeben", width = 10, command = callback)
+    #Berechung ymax --> Fenstergröße
     ymax = xmax - xmin + ymin
     deltacalc()
     #Durchgehen aller x-Werte
@@ -41,5 +50,9 @@ while True:
         #Durchgehen aller y-Werte
         for y in range(0, 500):
             #Berechnung ci
-    
+            
+            #farbe wählen
+            #Rechteck: 1. 2 Koord. Startpunkt, 2. 2 Koord. Endpunkt, von --> zu
+            w.create_rectangle(100, 100, 101, 101, fill=farbe)
+            
   w.pack()
