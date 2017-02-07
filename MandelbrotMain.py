c7b4699897be6d79bf5a485770b0a7cc40374cf8
joooomaaaa99 <jonasmartin99@gmx.de>
2017-02-07 16:31:37 +0100

    Update MandelbrotMain.py

--- MandelbrotMain.py
+++ MandelbrotMain.py
@@ -1,16 +1,33 @@
 
 """!/usr/bin/python
 -*- coding: UTF-8 -*-
-Date: 02.02.2017
+Date: 07.02.2017
 """
 
 import math 
 import tkinter
 
+xwidth = 500
+yheigt = 500
 
 fenster = tkinter.Tk()
-fenster.title("Die Mandelbrot-Menge")
+fenster.title("Die Mandelbrot-Menge.")
 w = tkinter.Canvas(fenster, width=xwidth, height=yheight)
 w.pack()
+#tk.mainloop()
 
-tk.mainloop()
+
+def callback():
+    w.get()
+    return()
+
+while True:
+  w.focus_set()
+  xmin = tkinter.Button(fenster, text = "xMin eingeben", width = 10, command = callback)
+  xmax = tkinter.Button(fenster, text = "xMax eingeben", width = 10, command = callback)
+  ymin = tkinter.Button(fenster, text = "yMin eingeben", width = 10, command = callback)
+  ymax = xmax - xmin + ymin
+  
+  
+  
+  w.pack()
